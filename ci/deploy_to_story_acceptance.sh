c806068664eac8a6f04d5ca032cf2164f7eb33ac
britz <jbritz@pivota.io>
2017-04-21 16:45:53 -0700

    Enforcing that SA deploys use sha's that passed CI
    [finishes #144138217]

--- ci/deploy_to_story_acceptance.sh
+++ ci/deploy_to_story_acceptance.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
 
+if [[ "$ShaToBuild" == "0" ]] ; then
+  echo "Cannot build from SHA: $ShaToBuild"
+  exit 1
+fi
+
+echo "Building SHA $ShaToBuild"
+git co $ShaToBuild
+
 shouldDeploy=$(git log HEAD~..HEAD | sed '1,/^$/d' | egrep "\[finishes\s+#[0-9]+\]|\[fixes\s+#[0-9]+\]|\[completes\s+#[0-9]+\]" | awk '{print}' )
 if [ ! -z "$shouldDeploy" ]
 then
@@ -8,7 +16,8 @@ then
     mvn clean package -DskipTests
 
     ./ci/deliver_tracker_story.sh
-    ./ci/add_story_to_manifest.sh manifest-acceptance.yml ${GIT_COMMIT}
+
+    ./ci/add_story_to_manifest.sh manifest-acceptance.yml ${ShaToBuild}
 
     cf login -a api.run.pivotal.io -u ${CF_USER} -p ${CF_PASSWORD} -s pronto -o dirk
     cf push -f manifest.yml -t 180
@@ -16,9 +25,10 @@ then
 
 
 else
-
     mkdir -p target
     touch target/fake-SNAPSHOT.jar
-
 fi
+
+git co master
+
 
